cddc59b43426a70a1686cb9b724b20fc2d93e749
Joao Thomaz <joaothomazti@hotmail.com>
2025-01-19 14:52:39 -0300

    Create CI

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://serverest.dev',
-    specPattern: "cypress/api/**/*.cy.{js,jsx,ts,tsx}"
+    specPattern: "cypress/api/**/*.cy.{js,jsx,ts,tsx}",
+    reporter: 'mochawesome', 
+    reporterOptions: {
+      reportDir: 'cypress/reports', 
+      overwrite: false,  
+      html: false, 
+      json: true, 
+    },
   },
 });
