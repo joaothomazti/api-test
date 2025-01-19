c27fc1dba73825127a21fcbf0d4b18d9c77a1bf8
Joao Thomaz <joaothomazti@hotmail.com>
2025-01-19 15:16:00 -0300

    update cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://serverest.dev',
     specPattern: "cypress/api/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: false,
     reporter: 'mochawesome', 
     reporterOptions: {
       reportDir: 'cypress/reports', 
